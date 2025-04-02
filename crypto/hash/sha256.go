aaf3eb9ef091d9eda9fce74dfcd323bc0d5f2d23
dyam <dyam.marcano@gmail.com>
2025-04-02 11:33:39 -0300

    rename hashing functions to improve clarity and add unit tests for SHA256

--- crypto/hash/sha256.go
+++ crypto/hash/sha256.go
@@ -5,13 +5,13 @@ import (
 	"encoding/hex"
 )
 
-func HashSHA256(data string) string {
+func Sha256(data string) string {
 	h := sha256.New()
 	h.Write([]byte(data))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func HashSHA256Bytes(data []byte) string {
+func Sha256Bytes(data []byte) string {
 	h := sha256.New()
 	h.Write(data)
 	return hex.EncodeToString(h.Sum(nil))
