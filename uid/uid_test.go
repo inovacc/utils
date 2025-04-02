830520223218d9417ae98f05688051825d990101
dyam <dyam.marcano@gmail.com>
2025-04-02 11:35:28 -0300

    add unit tests for UUID and KSUID generation functions

---
+++ uid/uid_test.go
@@ -0,0 +1,33 @@
+package uid
+
+import (
+	"github.com/google/uuid"
+	"github.com/inovacc/ksuid"
+	"testing"
+)
+
+func TestGenerateUUID(t *testing.T) {
+	v := GenerateUUID()
+	if len(v) != 36 {
+		t.Errorf("Expected UUID length of 36, got %d", len(v))
+		return
+	}
+
+	if _, err := uuid.Parse(v); err != nil {
+		t.Errorf("Expected valid UUID, got error: %v", err)
+		return
+	}
+}
+
+func TestGenerateKSUID(t *testing.T) {
+	v := GenerateKSUID()
+	if len(v) != 27 {
+		t.Errorf("Expected KSUID length of 27, got %d", len(v))
+		return
+	}
+
+	if _, err := ksuid.Parse(v); err != nil {
+		t.Errorf("Expected valid KSUID, got error: %v", err)
+		return
+	}
+}
