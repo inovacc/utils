4a19e42d594e4f797ed597bcebc80004bd71f793
dyam <dyam.marcano@gmail.com>
2025-04-02 12:40:53 -0300

    add encoding and decoding functions for gob serialization

--- encode/gob/gob.go
+++ encode/gob/gob.go
@@ -1,6 +1,22 @@
 package gob
 
+import (
+	"bytes"
+	"encoding/gob"
+)
+
 func EncodeGob(data any) ([]byte, error) {
-	// Implement the encoding logic here
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(data); err != nil {
+		return nil, err
+	}
 	return nil, nil
+}
+
+func DecodeGob(data []byte, v any) error {
+	buf := bytes.NewBuffer(data)
+	if err := gob.NewDecoder(buf).Decode(v); err != nil {
+		return err
+	}
+	return nil
 }
