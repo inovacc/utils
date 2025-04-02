96cf36c7389f7e82e73f5ad050f43110fbf80105
dyam <dyam.marcano@gmail.com>
2025-04-02 12:41:07 -0300

    add file handling functions for reading and writing data

---
+++ file/file.go
@@ -0,0 +1,43 @@
+package file
+
+import (
+	"bytes"
+	"os"
+)
+
+func WriteToFile(filename string, data []byte) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(data); err != nil {
+		return err
+	}
+
+	if err := file.Sync(); err != nil {
+		return err
+	}
+
+	if err := file.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadFromFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(file); err != nil {
+		return nil, err
+	}
+
+	if err := file.Close(); err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
